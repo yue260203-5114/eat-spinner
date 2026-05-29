7defbcc8244f826ced325379ef1424a429bda0d4
Lrishy5114 <yue260203@gmail.com>
2026-05-29 15:55:09 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'eat-spinner-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
